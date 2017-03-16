d9144d93515dd095f7f83d752145142d8444b0d0
yoko978 <0909978@gmail.com>
2017-03-16 23:47:31 +0000

    add user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  
   def show
     @user = User.find(params[:id])
   end
@@ -14,12 +15,31 @@ class UsersController < ApplicationController
       redirect_to @user
     else
       render 'new'
+    end # ここにendが必要です。
   end
-end
-
+  
+  def edit
+    if  @user = User.find(params[:id])
+      　redirect_to root_path unless(current_user == @user)
+    else
+       render 'edit'
+    end
+  
+  def update
+    if @user.update_attributes(user_params)
+      redirect_to @user
+      flash[:success] = "Update your profile"
+    else
+      render'edit'
+    end # ここにendが必要です。
+    
+# end # classをここで閉じてしまっていますので、これはいりません。
+  
   private
+
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+                                 :password_confirmation, :area, :profile)
   end
+# end # このendはいりません。
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,14 @@
 module UsersHelper
-    def gravater_for(user,options = {size: 80 })
-        gravater_id = Digest::MD5::hexdigest(user.email.downcase)
+    # def gravater_for(user,options = {size: 80 })
+    #     gravater_id = Digest::MD5::hexdigest(user.email.downcase)
+    #     size = options[:size]
+    #     gravatar_id = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    #     image_tag(gravater_id, alt: user.name, class:"gravatar")
+    # end
+    def gravatar_for(user, options = { size: 80 })
+        gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
         size = options[:size]
-        gravatar_id = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-        image_tag(gravater_id, alt: user.name, class:"gravatar")
+        gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+        image_tag(gravatar_url, alt: user.name, class: "gravatar")
     end
 end
